91d10c6341f5d12a345381b7899aee3f3f5a3a9e
Арсений <a.karavaichenko@onpoint.ru>
2022-04-17 00:51:00 +0300

    fix modules

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from 'react'
-import {Header, Footer}
+
+import {Header, Footer, Avatar}
  from "./components"
 
 
 const App = () => {
+
+  const handleAvatarClick = () => console.log('avatar clicked')
+  const handlePrintClick = () => console.log('print clicked')
+
   return (
       <div className="ui-wrapper">
-        <Header />
+        <Header onClick={handlePrintClick}/>
         <div className="ui-content-wrapper">
           <section className="ui-section">
             <div className="ui-container">
+              <Avatar onClick={handleAvatarClick}/>
               <h1 className="ui-title-1">Hello world!</h1>
             </div>
           </section>
         </div>
         <Footer />
-
       </div>
   )
 }
