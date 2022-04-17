a4f23bf04fcde7ab7ccdf7d9e9dcf213911b5468
Арсений <a.karavaichenko@onpoint.ru>
2022-04-17 19:26:17 +0300

    add component descr

--- src/components/UI/Description/Description.jsx
+++ src/components/UI/Description/Description.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
 import propTypes from 'prop-types'
+import cx from 'classnames'
 
-
-const Description = () => {
+const Description = ({isPrimary, isSecondary, className, children,  ...attrs}) => {
+  const classes= cx('ui-text', className, {
+    isPrimary,
+    isSecondary,
+  })
   return (
-      <p>{}</p>
+      <p className={classes}
+         contentEditable
+         suppressContentEditableWarning
+         spellCheck={false}
+         {...attrs}
+      >
+        {children}
+      </p>
   )
+}
+Description.propTypes = {
+  isPrimary: propTypes.bool,
+  isSecondary: propTypes.bool,
+  className: propTypes.string,
+  children: propTypes.node.isRequired,
+}
+
+Description.defaultProps = {
+  isPrimary: false,
+  isSecondary: false,
+  className: '',
+  // children: null,
+  // возникнет ошибка при вызове пропса без элементов внутри aka пустого пропса
 }
 
 export default Description
